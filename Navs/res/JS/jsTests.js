963f7dedf3a645ffd0cd7785d659a0c6c633b4fe
Andrei <kostychik@bk.ru>
2021-08-08 00:14:53 +0300

    +Flexbox 2 Examples

---
+++ Navs/res/JS/jsTests.js
@@ -0,0 +1,16 @@
+{/* <nav class="nav1">
+    <ul>
+
+        <li> <a href="Home.html">Home </a> </li>
+        <li> <a href="Flexbox.html">Flexbox </a> </li>
+        <li> <a href="">Linka3 </a> </li>
+        <li> <a href="">Linka4 </a> </li>
+        <li> <a href="">Linka5 </a> </li>
+        <li> <a href="">Linka6 </a> </li>
+        <li> <a href="Exaples.html">Examles </a> </li>
+
+    </ul>
+</nav> */}
+document.write("<h2>Первая программа на JavaScript</h2>"); // выводим заголовок
+document.write("Привет мир!");
+console.log(`5 * 12 = ${5 * 12}`);

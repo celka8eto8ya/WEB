63057d76d38644bf1606c927d12a32059adc8440
celka8eto8ya <kostychik@bk.ru>
2021-12-28 00:33:59 +0300

    Slider has done.

--- 3rdTemplate/res/js/script.js
+++ 3rdTemplate/res/js/script.js
@@ -12,6 +12,38 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.article_slider').slick();
+    $('.article_slider').slick({
+        arrows: true,
+        dots: false,
+        adaptiveHeight: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 1200, // swipe speed
+        easing: 'ease',
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3300,
+        pauseOnFocus: true, // pause autoplay
+        pauseOnHover: true, // pause autoplay
+        draggable: false, // swipe with help mouse
+        waitForAnimate: true, // waining end animation
+        responsive:
+            [
+                {
+                    breakpoint: 768,
+                    settings:
+                    {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings:
+                    {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+    });
 });
 
